d6882a43649f49b8ad7cb9b63b88591cf351053f
nishanthkorupolu <korupolu.nishanth@gmail.com>
2020-10-10 15:03:59 +0530

    Player Transformation

--- BGJ-3/Assets/XclaimR/Scripts/PlayerMovement.cs
+++ BGJ-3/Assets/XclaimR/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpHeight = 10f;
     //[SerializeField] float jumpSpeed = 5f;
 
-    [SerializeField]private bool isGrounded = false;
+    [SerializeField]public bool isGrounded = false;
 
     Vector3 vector;
 
